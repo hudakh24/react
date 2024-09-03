54df3da5afbebce63d7fefc965d1e1bcc5cad9a1
hudakh24 <hudashoaib2000@gmail.com>
2024-09-03 13:46:42 +0500

    Front-End

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
 import Counter from "./components/Counter";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
   const [userInput, setUserInput] = useState(""); //store input into state
   const [items, setItems] = useState(["Jeans", "Shirt"]);
+  const [deleteCheck, setDeleteCheck] = useState(false);
 
   const handleInputChange = (e) => {
     setUserInput(e.target.value);
   };
   const addItem = () => {
+    if (!userInput) {
+      return;
+    }
     setItems([...items, userInput]); // Add new item to the list
     setUserInput(""); // Clear the input field
   };
+
+  const deleteItem = (index) => {
+    //let wantToDelete = prompt("Want to delete??");
+    //if (wantToDelete === "yes") {
+    const updatedItems = [...items.slice(0, index), ...items.slice(index + 1)];
+    setItems(updatedItems);
+    setDeleteCheck(!deleteCheck);
+    // } else {
+    //   return;
+    // //}
+  };
+
+  //alert
+  useEffect(() => {
+    const newItem = prompt("Please enter the item yo want to add");
+    if (!newItem) {
+      return; //exit function
+    }
+    setItems([...items, newItem]);
+  }, []);
 
   return (
     <>
@@ -22,11 +46,14 @@ const App = () => {
       />
       <button onClick={addItem}>Add Item</button>
       {items.map((item, index) => (
-        <Counter key={index} itemName={item} />
+        <Counter
+          key={index} //Always pass unique ID in key  if want manipulation  & if we want to display only then we can use index
+          itemName={item}
+          onDelete={() => deleteItem(index)} // Pass delete function
+        />
       ))}
 
-      {/* <Counter itemName="Jeans"></Counter>
-      <Counter itemName="Jackets"></Counter>
+      {/* <Counter itemName="Jackets"></Counter>
       <Counter itemName="Shirts"></Counter> */}
     </>
   );
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
-const Counter = ({ itemName }) => {
+const Counter = ({ itemName, onDelete }) => {
   // let count = 5;
   const [count, setCount] = useState(0);
 
@@ -13,18 +13,28 @@ const Counter = ({ itemName }) => {
     let value = count - 1;
     setCount(value);
   };
+
+  useEffect(() => {
+    console.log("userEffect with count", count);
+    return () => {
+      console.log("cleanUp function", count);
+    };
+  }, [count]);
+
   return (
     <>
       <h1>{itemName}</h1>
       <h1>{count}</h1>
       <button onClick={Increment}>Increment</button>
       <button onClick={Decrement}>Decrement</button>
+      <button onClick={onDelete}>Delete</button>
     </>
   );
 };
 
 Counter.propTypes = {
   itemName: PropTypes.string.isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
 export default Counter;
